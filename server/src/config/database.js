eacfb21f7438f8fffed5ae890a32bb6c453d97c5
Mauricio Pereira <mauriciojuniordesigner@gmail.com>
2020-10-10 18:39:38 -0300

    estrutura basica do backend, conexao com banco e migrations de usuario

---
+++ server/src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "host": "localhost",
+  "username": "root",
+  "password": "senha",
+  "database": "castra_pet",
+  "dialect": "mysql",
+  "operatorsAliases": 0,
+  "logging": false,
+  "define": {
+    "timestamps": true,
+    "underscored": true
+  }
+}
