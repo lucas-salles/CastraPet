6f9b01262795824c25d9bfe74bc13f95f912aea1
Lucas Sales <lucassales398@gmail.com>
2020-10-19 19:34:08 -0300

    Algumas correções nas rotas e coloquei o dialect do db para ser lido do .env

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,15 +1,15 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
-  "host": process.env.DB_HOST,
-  "username": process.env.DB_USER,
-  "password": process.env.DB_PASSWORD,
-  "database": process.env.DB_DATABASE,
-  "dialect": "mysql",
-  "operatorsAliases": 0,
-  "logging": false,
-  "define": {
-    "timestamps": true,
-    "underscored": true
-  }
-}
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  dialect: process.env.DB_DIALECT,
+  operatorsAliases: 0,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const routes = express.Router()
+const express = require("express");
+const routes = express.Router();
 
-const UserController = require('../controllers/UserController')
+const UserController = require("../controllers/UserController");
 
 // POST
-routes.post('/:id', UserController.update)
-routes.post('/', UserController.store)
+routes.post("/", UserController.store);
 
 // GET
-routes.get('/:id', UserController.find)
-routes.get('/', UserController.index)
+routes.get("/:id", UserController.find);
+routes.get("/", UserController.index);
 
 // PUT
-routes.put('/:id', UserController.delete)
+routes.put("/:id", UserController.update);
+
+// DELETE
+routes.delete("/:id", UserController.delete);
 
 module.exports = routes;
